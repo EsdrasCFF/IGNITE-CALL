4ec2269c415eaf82acdad62307967ab9cb3746d0
EsdrasCFF <ecff.dev@gmail.com>
2023-12-06 22:58:04 -0300

    feat: Added validation to input with zod

--- src/app/(home)/components/ClaimUsernameForm.tsx
+++ src/app/(home)/components/ClaimUsernameForm.tsx
@@ -1,17 +1,51 @@
 "use client"
 
-import { Box, Button, TextInput } from "@ignite-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, TextInput, Text } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const claimUsernameFormSchema = z.object({
+  username: z.string()
+    .min(3, {message: "O usuário precisa ter pelo menos 3 letras."})
+    .regex(/^([a-z\\-]+)$/i, {message: "O usuário pode ter apenas letras e hífens"})
+    .transform(username => username.toLowerCase())
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm () {
-  return (
-    <form className="grid grid-cols-[1fr,auto] gap-2" >
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario"/>
+  const { register, handleSubmit, formState: {errors} } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema)
+  })
 
-      <Button size='sm' type='submit'>
-        Reservar 
-        <ArrowRight/>
-      </Button>
-    </form>
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <form className="grid grid-cols-[1fr,auto] gap-2" onSubmit={handleSubmit(handlePreRegister)} >
+        <TextInput  
+          size="sm" 
+          prefix="ignite.com/" 
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+
+        <Button size='sm' type='submit'>
+          Reservar 
+          <ArrowRight/>
+        </Button>
+        
+      </form>
+      
+      <div className="mt-2">
+        <Text size="sm" >
+          {errors.username ? errors.username.message : 'Digite o nome de usuário desejado'}
+        </Text>
+      </div>
+    </>
   );
 }
