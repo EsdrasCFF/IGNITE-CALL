6f60af00d786a247dbc86d92b666c94486210fa8
EsdrasCFF <ecff.dev@gmail.com>
2023-12-07 12:46:35 -0300

    fix: prop sent to component

--- src/app/register/components/Header.tsx
+++ src/app/register/components/Header.tsx
@@ -1,14 +1,14 @@
+import { ReactNode } from "react";
 
-export function Header() {
+interface HeaderProps {
+  children: ReactNode;
+}
+
+
+export function Header({children}: HeaderProps) {
   return (
-    <div className="py-0 px-4" >
-      <h3>
-
-      </h3>
-
-      <p className="text-gray200 mb-6" >
-
-      </p>
+    <div className="py-0 px-6" >
+      {children}
     </div>
   );
 }
