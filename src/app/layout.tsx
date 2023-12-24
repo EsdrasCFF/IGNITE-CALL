eb837d418ed8b0288487691e188ce0128f8d1c2b
EsdrasCFF <ecff.dev@gmail.com>
2023-12-24 17:58:42 -0300

    feat: using AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
+import { AuthProvider } from '@/providers/auth'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   )
