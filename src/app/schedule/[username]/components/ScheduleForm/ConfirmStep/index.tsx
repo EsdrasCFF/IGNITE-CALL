53afedd876f451fd44d031903a1bdf17cedf6df7
EsdrasCFF <ecff.dev@gmail.com>
2024-01-07 16:39:43 -0300

    feat: add validation scheme to Form with zod

--- src/app/schedule/[username]/components/ScheduleForm/ConfirmStep/index.tsx
+++ src/app/schedule/[username]/components/ScheduleForm/ConfirmStep/index.tsx
@@ -3,14 +3,30 @@ import { ConfirmForm } from "./ConfirmForm";
 import { FormHeader } from "./FormHeader";
 import { Button, TextArea, TextInput } from "@ignite-ui/react";
 import { FormActions } from "./FormActions";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormError } from "./FormError";
+
+const ConfirmFormSchema = z.object({
+  name: z.string().min(3,{message: 'O nome precisa de pelo menos 3 caracteres!'}),
+  email: z.string().email({message: 'Digite um e-mail válido!'}),
+  observations: z.string().nullable(),
+})
+
+type ConfirmFormData = z.infer<typeof ConfirmFormSchema>
 
 export function ConfirmStep() {
-  function handleConfirmScheduling() {
+  const { register, handleSubmit, formState: {isSubmitting, errors} } = useForm<ConfirmFormData>({
+    resolver: zodResolver(ConfirmFormSchema)
+  })
+
+  function handleConfirmScheduling(data: ConfirmFormData) {
 
   }
 
   return (
-    <ConfirmForm onSubmit={handleConfirmScheduling}>
+    <ConfirmForm onSubmit={handleSubmit(handleConfirmScheduling)}>
       <FormHeader>
         <p className="flex items-center gap-2" >
           <CalendarBlank color="#A9A9B2" width={18} height={18} />
@@ -25,22 +41,24 @@ export function ConfirmStep() {
 
       <label className="flex flex-col gap-2" >
         <p>Nome Completo</p>
-        <TextInput  placeholder="Seu nome"/>
+        <TextInput {...register('name')} placeholder="Seu nome"/>
+        {errors.name && (<FormError> {errors.name.message} </FormError>)}
       </label>
       
       <label className="flex flex-col gap-2" >
         <p>Endereço de e-mail</p>
-        <TextInput type="email" placeholder="jhondoe@example.com"/>
+        <TextInput {...register('email')} type="email" placeholder="jhondoe@example.com"/>
+        {errors.email && (<FormError> {errors.email.message} </FormError>)}
       </label>
       
       <label className="flex flex-col gap-2" >
         <p>Observações</p>
-        <TextArea/>
+        <TextArea {...register('observations')} />
       </label>
 
       <FormActions>
         <Button type="button" variant="tertiary">Cancelar</Button>
-        <Button type="submit">Confirmar</Button>
+        <Button type="submit" disabled={isSubmitting} >Confirmar</Button>
       </FormActions>
     </ConfirmForm>
   )
