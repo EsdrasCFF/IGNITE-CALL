e04040c31d493fdb1eaaa46cc25f59ce66d58ecb
EsdrasCFF <ecff.dev@gmail.com>
2024-01-07 02:26:29 -0300

    feat: render TimerPicker in the CalendarStep component

--- src/app/schedule/[username]/components/ScheduleForm/CalendarStep.tsx
+++ src/app/schedule/[username]/components/ScheduleForm/CalendarStep.tsx
@@ -1,15 +1,49 @@
 import { Calendar } from "@/components/Calendar";
 import { Box } from "@ignite-ui/react";
 import { ReactNode } from "react";
+import { TimePicker } from "@/components/TimePicker";
+import { TimePickerHeader } from "@/components/TimePicker/TimePickerHeader";
+import { TimePickerList } from "@/components/TimePicker/TimePickerList";
+import { TimePickerItem } from "@/components/TimePicker/TimePickerItem";
 
 interface CalendarStepProps {
   children: ReactNode;
 }
 
 export function CalendarStep() {
+  const isDateSelected = true
+  const isTimePickerOpen = isDateSelected
+
   return(
-    <Box className="grid grid-cols-[1fr] mx-0 mt-6 mb-0 p-0 relative max-w-full w-[540px]" >
+    <Box className={`
+      grid grid-cols-[1fr] mx-0 mt-6 mb-0 p-0 relative max-w-full 
+      ${isTimePickerOpen ? 'time-picker-open' : 'w-[540px]'}
+      `}>
       <Calendar/>
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span className="text-gray200">07 de Janeiro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>12:00</TimePickerItem>
+            <TimePickerItem>13:00</TimePickerItem>
+            <TimePickerItem>14:00</TimePickerItem>
+            <TimePickerItem>15:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>17:00</TimePickerItem>
+            <TimePickerItem>18:00</TimePickerItem>
+            <TimePickerItem>19:00</TimePickerItem>
+            <TimePickerItem>20:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Box>
   )
 }
--- src/components/Calendar/CalendarDay.tsx
+++ src/components/Calendar/CalendarDay.tsx
@@ -8,9 +8,10 @@ interface CalendarDayProps {
 export function CalendarDay({children, disabled=false}: CalendarDayProps) {
   return (
     <button  disabled={disabled} className="
-      w-full aspect-square bg-gray600 text-center cursor-pointer rounded-md
-      disabled:bg-none disabled:cursor-not-allowed disabled:opacity-40 
-      focus:outline focus:text-gray100" >
+      w-full aspect-square bg-gray600 text-center cursor-pointer rounded-md 
+      focus:outline focus:text-gray100
+      disabled-button
+      ">
       {children}
     </button>
   )
