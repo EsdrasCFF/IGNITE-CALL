7c0aace93d2365c358209193bf6ed89582a375e0
EsdrasCFF <ecff.dev@gmail.com>
2024-01-17 22:11:31 -0300

    feat: add disabled props in this component

--- src/components/TimePicker/TimePickerItem.tsx
+++ src/components/TimePicker/TimePickerItem.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 
 interface TimerPickerItemProps {
   children: ReactNode;
+  disabled?: boolean; 
 }
 
-export function TimePickerItem({children}: TimerPickerItemProps) {
+export function TimePickerItem({children, disabled}: TimerPickerItemProps) {
   return (
-    <button className="border-[0] bg-gray600 py-2 px-0 cursor-pointer text-gray100 rounded-sm font-medium leading-normal disabled-button" >
+    <button disabled={disabled} className="border-[0] bg-gray600 py-2 px-0 cursor-pointer text-gray100 rounded-sm font-medium leading-normal disabled-button" >
       {children}
     </button>
   )
