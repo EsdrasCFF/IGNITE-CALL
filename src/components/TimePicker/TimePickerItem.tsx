fdbe21205abacebb302cec6150c53109db2da9cd
EsdrasCFF <ecff.dev@gmail.com>
2024-01-20 16:06:27 -0300

    feat: add onClick props in the TimePickerComponent

--- src/components/TimePicker/TimePickerItem.tsx
+++ src/components/TimePicker/TimePickerItem.tsx
@@ -2,12 +2,17 @@ import { ReactNode } from "react";
 
 interface TimerPickerItemProps {
   children: ReactNode;
-  disabled?: boolean; 
+  disabled?: boolean;
+  onClick: () => void
 }
 
-export function TimePickerItem({children, disabled}: TimerPickerItemProps) {
+export function TimePickerItem({children, disabled, onClick}: TimerPickerItemProps) {
   return (
-    <button disabled={disabled} className="border-[0] bg-gray600 py-2 px-0 cursor-pointer text-gray100 rounded-sm font-medium leading-normal disabled-button" >
+    <button 
+      disabled={disabled} 
+      className="border-[0] bg-gray600 py-2 px-0 cursor-pointer text-gray100 rounded-sm font-medium leading-normal disabled-button" 
+      onClick={onClick}
+    >
       {children}
     </button>
   )
