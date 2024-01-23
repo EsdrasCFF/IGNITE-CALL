5b66ed99ab53592f76bf10f44cf336814f24d8a1
EsdrasCFF <ecff.dev@gmail.com>
2024-01-23 10:46:16 -0300

    chore: add tailwindcss pluig-in tupography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,7 @@ const config: Config = {
     },
   },
   plugins: [
+    require('@tailwindcss/typography'),
     function ({ addComponents }: { addComponents: (components: Record<string, any>) => void }) {
       addComponents({
         '.max-width-custom': {
